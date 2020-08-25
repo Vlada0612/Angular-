9dd2c6861d10dac388a3e7d174b28de8db23c4d7
Vlada Sinitsina <vsinitsina@itransition.com>
2020-08-25 14:15:13 +0300

    Demo-service

---
+++ demo-service/src/main/java/com/example/demoservice/DemoServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class DemoServiceApplication {
+
+    public static void main(String[] args) {
+        run(DemoServiceApplication.class, args);
+    }
+
+}
---
+++ demo-ui/src/app/dnd.directive.ts
@@ -0,0 +1,45 @@
+import {Directive, EventEmitter, HostBinding, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appDnd]'
+})
+
+export class DndDirective {
+
+  private static LIGHT_COLOR = '#f5fcff';
+  private static DEFAULT_OPACITY = '1';
+  private static EVENT = '$event';
+
+  @Output() onFileDropped = new EventEmitter<any>();
+
+  @HostBinding('style.background-color') private background = DndDirective.LIGHT_COLOR;
+  @HostBinding('style.opacity') private opacity = DndDirective.DEFAULT_OPACITY;
+
+  //Dragover listener
+  @HostListener('dragover', [DndDirective.EVENT]) onDragOver(evt) {
+    this.setParameters(evt, '#9ecbec', '0.8');
+  }
+
+  //Dragleave listener
+  @HostListener('dragleave', [DndDirective.EVENT])
+  public onDragLeave(evt) {
+    this.setParameters(evt, DndDirective.LIGHT_COLOR, DndDirective.DEFAULT_OPACITY);
+  }
+
+  //Drop listener
+  @HostListener('drop', [DndDirective.EVENT])
+  public ondrop(evt) {
+    this.setParameters(evt, DndDirective.LIGHT_COLOR, DndDirective.DEFAULT_OPACITY);
+    let files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.onFileDropped.emit(files);
+    }
+  }
+
+  private setParameters(evt, background, opacity) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background = background;
+    this.opacity = opacity;
+  }
+}
